4ff40e57a5ad17280a271ceef7803d72aab028b5
Serhii-Leonenko <leonenko3149@gmail.com>
2026-06-04 09:29:11 +0300

    added urls

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,13 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
+api_v1_patterns = [
+    path("", include("users.urls")),
+    path("", include("messenger.urls")),
+]
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/v1/", include("messenger.urls"))
+    path("api/v1/", include(api_v1_patterns)),
 ] + debug_toolbar_urls()
