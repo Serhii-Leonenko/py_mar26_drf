e8e1c33805cf75e8e007146f7485b9716aa6fb38
Serhii-Leonenko <leonenko3149@gmail.com>
2026-06-04 10:06:03 +0300

    added password hashing

--- users/serializers.py
+++ users/serializers.py
@@ -8,4 +8,13 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username")
+        fields = ("id", "username", "password")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 6
+            }
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
